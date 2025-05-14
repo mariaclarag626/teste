a7495e6fe1030daa1123db1bae9de6939cebaeaa
mariaclara <maria.calvarenga@eaportal.org>
2025-05-14 11:48:31 -0300

    atualizacao 14/05

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -2,6 +2,8 @@ function Footer() {
   return (
     <footer className="bg-gray-200 text-center py-2 text-sm text-gray-700 mt-8" style={{display:'flex', justifyContent:'center', alignItems:'center', flexDirection:'column', marginTop:'20px'}}>
       © 2025 - Feito por Maria Clara
+    
+      <br/>AV1 – Mini-Projeto de Roteamento e Consumo de API – DSW – 2°Bimestre
     </footer>
   );
 }
--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,7 +1,8 @@
 function Header() {
   return (
     <header className="bg-blue-600 text-white py-3 text-center text-lg font-medium" style={{display:'flex', justifyContent:'center', alignItems:'center', flexDirection:'column'}}>
-      Meu Site React
+      Meu Site React 
+      <h2>Bem-vindos ao site que você recebe conselhos todos os dias, os quais irão melhorar o seu dia.</h2>
     </header>
   );
 }
